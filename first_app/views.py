89ebe1cd8eaffa218582faef2ce948b6cc98a68e
shufolislam1 <shufolislam1255@gmail.com>
2024-02-13 20:56:14 +0600

    half login done

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from . import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -7,8 +10,29 @@ def register(request):
         register_form = forms.RegistrationForm(request.POST)
         if register_form.is_valid():
             register_form.save()
+            messages.success(request,'registered successful')
             return redirect('register')
 
     else:
         register_form = forms.RegistrationForm()
-    return render(request, 'register.html', {'form':register_form})
+    return render(request, 'register.html', {'form':register_form, 'type':'register'})
+
+def userLogin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.changed_data['password']
+            user_authenticate = authenticate(username=user_name, password = user_pass)
+            if user_authenticate is not None:
+                messages.success(request,'login successful')
+                login(request, user_authenticate)
+                return redirect('profile')
+            else:
+                messages.warning(request, 'login info incorrect')
+                return redirect('register')
+            
+    else:
+        form = AuthenticationForm()
+        return render(request,'register.html', {'form': form, 'type':'login'})
+            
