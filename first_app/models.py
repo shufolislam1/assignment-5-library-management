09d8acd282e1cee8f396237b20afc068aaa3f318
shufolislam1 <shufolislam1255@gmail.com>
2024-02-14 22:19:03 +0600

    model creaated

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Car(models.Model):
+    brand = models.ForeignKey(Brand,on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # image = models.ImageField()
+    
+    def __str__(self):
+        return self.name
